2644c14f77d19b124ec8241d5c7d067d8ea44bcd
vebjorn1997 <11070506@uvu.edu>
2025-11-15 15:53:11 -0700

    second

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/vebjorn1997.github.io' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/vebjorn1997.github.io' : '',
+  images: {
+    unoptimized: true, // Required for static export
+  },
 };
 
 export default nextConfig;
